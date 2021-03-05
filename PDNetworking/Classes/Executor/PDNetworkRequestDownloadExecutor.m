60b41e2582af304fec5678a5fb273260d668fb1b
liang <leiliang4@xdf.cn>
2021-03-05 12:58:54 +0800

    optimize download logic

--- PDNetworking/Classes/Executor/PDNetworkRequestDownloadExecutor.m
+++ PDNetworking/Classes/Executor/PDNetworkRequestDownloadExecutor.m
@@ -16,7 +16,42 @@
             !self.request.downloadProgress ?: self.request.downloadProgress(downloadProgress);
         });
     } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
-        return self.request.destination(targetPath, response);
+        // Get 'Content-Length' from response header
+        long long contentLen = -1;
+        
+        if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+            NSHTTPURLResponse *HTTPResp = (NSHTTPURLResponse *)response;
+            if (HTTPResp.statusCode >= 300 && HTTPResp.statusCode < 600) {
+                return nil;
+            }
+
+            NSString *contentLenText = HTTPResp.allHeaderFields[@"Content-Length"];
+            contentLen = [contentLenText longLongValue];
+        }
+
+        if (contentLen <= 0) {
+            // Maybe `expectedContentLength` value is `-1`, if the resource type is zip, should set "Accept-Encoding" to request header
+            // https://stackoverflow.com/questions/11136020/response-expectedcontentlength-return-1
+            contentLen = response.expectedContentLength;
+        }
+                
+        if (contentLen <= 0) { return nil; }
+        
+        // 'fileSize' from local file info
+        NSDictionary *fileInfo = [[NSFileManager defaultManager] attributesOfItemAtPath:targetPath.path error:NULL];
+        long long fileSize = [fileInfo[NSFileSize] longLongValue];
+        if (fileSize < contentLen) { return nil; }
+        
+        // Create intermediate dir path if needed
+        NSURL *fileURL = self.request.destination(targetPath.path, response);
+        BOOL isDir = NO; NSString *dirPath = [fileURL.path stringByDeletingLastPathComponent];
+        BOOL dirExist = [[NSFileManager defaultManager] fileExistsAtPath:dirPath isDirectory:&isDir];
+        
+        if (!dirExist || !isDir) {
+            [[NSFileManager defaultManager] createDirectoryAtPath:dirPath withIntermediateDirectories:YES attributes:nil error:nil]
+        }
+        
+        return fileURL;
     } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
         [self _handleResponse:filePath error:error];
     }];;
