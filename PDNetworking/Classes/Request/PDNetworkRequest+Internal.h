bb989b0fe5fbe59957d588aa02eb58e1928557f1
liang <leiliang4@xdf.cn>
2021-03-05 14:52:03 +0800

    remove unused class name

--- PDNetworking/Classes/Request/PDNetworkRequest+Internal.h
+++ PDNetworking/Classes/Request/PDNetworkRequest+Internal.h
@@ -10,8 +10,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class PDNetworkRequestVisitor;
-
 typedef NS_ENUM(NSUInteger, PDNetworkRequestType) {
     PDNetworkRequestTypeRegular   = 0,
     PDNetworkRequestTypeDownload  = 1,
